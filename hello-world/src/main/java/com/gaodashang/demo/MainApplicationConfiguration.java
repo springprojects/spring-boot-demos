0dad3eb531d47c0ae13dbd7351bef564162ac49a
eva <eva@nef.com>
2015-03-19 15:21:49 +0800

    添加配置文件的功能

--- hello-world/src/main/java/com/gaodashang/demo/MainApplicationConfiguration.java
+++ hello-world/src/main/java/com/gaodashang/demo/MainApplicationConfiguration.java
@@ -4,19 +4,30 @@ package com.gaodashang.demo;
  * Created by eva on 2015/3/7.
  */
 
-import org.springframework.boot.*;
-import org.springframework.boot.autoconfigure.*;
-import org.springframework.stereotype.*;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @EnableAutoConfiguration
 public class MainApplicationConfiguration {
 
+    @Value("${com.gaodashang.message:wo shi zhong guo ren.}")
+    private String message;
+
     @RequestMapping("/")
     @ResponseBody
     String home() {
         return "Hello World!";
+    }
+
+    @RequestMapping("/hello")
+    @ResponseBody
+    String hello() {
+        return message;
     }
 
     public static void main(String[] args) throws Exception {
